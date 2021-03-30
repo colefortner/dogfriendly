752192c2bd8b6c0b383170702ddcfbfa7892095b
colefortner <colefortner@gmail.com>
2021-03-30 12:57:42 -0400

    Third time is a charm, db fixed, populated correctly

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,5 +1,7 @@
 class Business < ApplicationRecord
     has_many :business_comments
+    has_many :users, through: :business_comments
     has_many :ratings
+    has_many :users, through: :ratings
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
     has_many :business_comments
+    has_many :businesses, through: :business_comments
     has_many :ratings
+    has_many :businesses, through: :ratings
 end
 
 
